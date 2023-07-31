3d55a4b5050646ca3c8432880055ca8664d5c974
drew-creator <andrew.n99@yahoo.com>
2023-07-31 13:34:01 -0700

    config added for mongoose and env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KudosModule } from './kudos/kudos/kudos.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { KudosSchema } from './schema/kudos.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    KudosModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      process.env.DB_CONNECTION_STRING, 
+      { dbName: process.env.DB_NAME }
+    )
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
